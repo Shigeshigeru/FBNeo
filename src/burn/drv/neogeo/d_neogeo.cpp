56695de5be1eb75d5baec54e3589fca673e1d072
Shigeshigeru <103879054+Shigeshigeru@users.noreply.github.com>
2025-03-10 01:48:46 +0900

    Update d_neogeo.cpp
    test

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -19836,6 +19836,7 @@ struct BurnDriver BurnDrvMslug4unity = {
 // For more information on how to make this set, please refer to this page.
 // https://note.com/shigeshigeru/n/nd653e59c1702
 // Japanese only
+
 	{ "263-s1d.s1",	0x020000, 0xc4f4ed0e, 2 | BRF_GRA },	// Fixed S1 data
 #define MSLUG4_DECRYPTED_SPR1								\
 	{ "263-c1d.c1",	0x800000, 0xa75ffcde, 3 | BRF_GRA },	\
