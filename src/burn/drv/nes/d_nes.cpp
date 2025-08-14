56ab4bfd91dc06db741c640bf7c641b59c0e6ce3
Shigeshigeru <103879054+Shigeshigeru@users.noreply.github.com>
2025-08-14 23:45:38 +0900

    Update d_nes.cpp
    dick

--- src/burn/drv/nes/d_nes.cpp
+++ src/burn/drv/nes/d_nes.cpp
@@ -54860,25 +54860,20 @@ struct BurnDriver BurnDrvnes_zunousengal = {
 	SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
 };
 
-
-// -----------------------------------------------------------------------------
-// FBneo NeCo Additional ROM Set
-// -----------------------------------------------------------------------------
-
-// Double Dragon II: The Revenge (USA)(rev1)
-static struct BurnRomInfo nes_doubldraiirev1RomDesc[] = {
-	{ "Double Dragon II - The Revenge (USA)(rev1)(1990)(Acclaim).nes",          262160, 0xa0f7027d, BRF_ESS | BRF_PRG },
+static struct BurnRomInfo nes_fishRomDesc[] = {
+	{ "Fish (HB)(2023)(Voxel).nes",          40976, 0x20120db7, BRF_ESS | BRF_PRG },
 };
 
-STD_ROM_PICK(nes_doubldraiirev1)
-STD_ROM_FN(nes_doubldraiirev1)
+STD_ROM_PICK(nes_fish)
+STD_ROM_FN(nes_fish)
 
-struct BurnDriver BurnDrvnes_doubldraiirev1 = {
-	"nes_doubldraiirev1", "nes_doubldraii", NULL, NULL, "1990",
-	"Double Dragon II: The Revenge (USA)(rev1)\0", NULL, "Acclaim Entertainment", "NES / Famicom",
+struct BurnDriver BurnDrvnes_fish = {
+	"nes_fish", NULL, NULL, NULL, "2023",
+	"Fish (HB)\0", NULL, "Voxel", "NES / Famicom",
 	NULL, NULL, NULL, NULL,
-	BDF_GAME_WORKING | BDF_CLONE, 2, HARDWARE_NES, GBF_SCRFIGHT, 0,
-	NESGetZipName, nes_doubldraiirev1RomInfo, nes_doubldraiirev1RomName, NULL, NULL, NULL, NULL, NESInputInfo, NESDIPInfo,
+	BDF_GAME_WORKING | BDF_HOMEBREW, 2, HARDWARE_NES, GBF_MISC, 0,
+	NESGetZipName, nes_fishRomInfo, nes_fishRomName, NULL, NULL, NULL, NULL, NESInputInfo, NESDIPInfo,
 	NESInit, NESExit, NESFrame, NESDraw, NESScan, &NESRecalc, 0x40,
 	SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
 };
+
