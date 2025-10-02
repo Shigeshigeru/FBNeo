94f541cbbfe2beeb1617edb9ea64fc26aab79b61
Shigeshigeru <103879054+Shigeshigeru@users.noreply.github.com>
2025-10-02 09:26:53 +0900

    mslug5 MVS,AES fix
    これで動くかな？
    ずっと間違えてた

--- src/burn/drv/neogeo/d_neogeo.cpp
+++ src/burn/drv/neogeo/d_neogeo.cpp
@@ -20734,7 +20734,7 @@ static INT32 mslug5dInit()
 {
 	INT32 nRet;
 
-	NeoCallbackActive->pInitialise = mslug5b2Callback;
+//	NeoCallbackActive->pInitialise = mslug5Callback;
 
 	nRet = NeoPVCInit();
 
@@ -30218,13 +30218,24 @@ static struct BurnRomInfo mslug5beRomDesc[] = {
 STDROMPICKEXT(mslug5be, mslug5be, neogeo)
 STD_ROM_FN(mslug5be)
 
+static INT32 mslug5beInit()
+{
+	INT32 nRet;
+
+	NeoCallbackActive->pInitialise = mslug5b2Callback;
+
+	nRet = NeoPVCInit();
+
+	return nRet;
+}
+
 struct BurnDriver BurnDrvmslug5be = {
 	"mslug5be", "mslug5", "neogeo", NULL, "2003",
 	"Metal Slug 5 (NGM-2680, Metal Slug Complete Version, bootleg)\0", NULL, "bootleg", "Neo Geo MVS",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_BOOTLEG, 2, HARDWARE_PREFIX_CARTRIDGE | HARDWARE_SNK_NEOGEO, GBF_RUNGUN, FBF_MSLUG,
 	NULL, mslug5beRomInfo, mslug5beRomName, NULL, NULL, NULL, NULL, neogeoInputInfo, neogeoDIPInfo,
-	mslug5dInit, NeoExit, NeoFrame, NeoRender, NeoScan, &NeoRecalcPalette,
+	mslug5beInit, NeoExit, NeoFrame, NeoRender, NeoScan, &NeoRecalcPalette,
 	0x1000,	304, 224, 4, 3
 };
 
